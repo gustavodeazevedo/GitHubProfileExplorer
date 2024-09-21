882396ea5cacd02af2b2a479665309b6fa24e9d1
Gustavo de Azevedo <gustavodeazevedo2003@outlook.com>
2024-09-20 21:28:18 -0300

    feat: Functions for obtaining user data

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 import { langColors } from './config';
 
-// Defina a URL base diretamente no código
 const api = axios.create({
   baseURL: 'https://api.github.com', // URL da API GitHub
 });
 
 // Função para obter informações do usuário
-export const getUser = async (login) => api.get(`/users/${login}`);
+export const getUser = async (login) => {
+  try {
+    return await api.get(`/users/${login}`);
+  } catch (error) {
+    throw new Error('Usuário não encontrado.');
+  }
+};
 
 // Função para obter repositórios do usuário
-export const getRepos = async (login) => api.get(`/users/${login}/repos`);
+export const getRepos = async (login) => {
+  try {
+    return await api.get(`/users/${login}/repos`);
+  } catch (error) {
+    throw new Error('Repositórios não encontrados.');
+  }
+};
 
 // Função para obter estatísticas das linguagens de programação
 export const getLangsFrom = (repositories) => {
